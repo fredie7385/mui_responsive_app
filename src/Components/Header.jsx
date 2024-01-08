c23f4bad9705a86b8b53f2beeb53e845e3b97b9c
Fredrick Kabiru <50455667+fredie7385@users.noreply.github.com>
2024-01-08 08:05:37 +0300

    3rd commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,35 @@
-import {
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { Avatar, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
-import { useTheme} from '@mui/material/styles';
+import { useTheme } from "@mui/material/styles";
 import { ColorModeContext } from "../App";
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => {
+  return {
+    toolbar: {
+      background: "purple",
+      color: "#dfdfdf",
+    },
+    avatar: {
+      marginRight: theme.spacing(2),
+    },
+  };
+});
 
 export default function Header(props) {
+  const classes = useStyles();
   const theme = useTheme();
   const colorMode = React.useContext(ColorModeContext);
 
   return (
-    <Toolbar>
+    <Toolbar className={classes.toolbar}>
+      <Avatar
+        className={classes.avatar}
+        src="https://images.pexels.com/photos/5215024/pexels-photo-5215024.jpeg?auto=compress&cs=tinysrgb&w=600"
+      />
       <IconButton
         color="inherit"
         aria-label="open drawer"
@@ -26,10 +40,14 @@ export default function Header(props) {
         <MenuIcon />
       </IconButton>
       <Typography variant="h5" sx={{ flexGrow: 1 }} component="div">
-        My web App
+      _Web App_
       </Typography>
-      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+      <IconButton onClick={colorMode.toggleColorMode} color="inherit">
+        {theme.palette.mode === "dark" ? (
+          <Brightness4Icon />
+        ) : (
+          <Brightness7Icon />
+        )}
       </IconButton>
     </Toolbar>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import { Avatar, Box, Card, Grid } from "@mui/material";
+import React from "react";
 
 export default function Home() {
+
+  const CARD_PROPERTIES ={
+    borderRadius:1.5,
+    boxShadow:1,
+    padding:2
+  }
+
   return (
-    <h1>Home</h1>
-  )
+    <>
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        md={6}
+        lg={4}
+        xl={3}
+        sx={{ width: "100%" }}
+      >
+        <Card sx={CARD_PROPERTIES} variant="outlined">
+          <Box
+          sx={{
+            display: "flex",
+            flexDirection:'column',
+            alignItems: 'center',
+            textAlign: 'center',
+            p:2
+          }}
+          >
+            <Avatar></Avatar>
+          </Box>
+        </Card>
+      </Grid>
+    </>
+  );
 }
